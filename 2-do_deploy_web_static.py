79ca3ca6e1fdd73d92506e944a6fdc280ae82b22
root <JacksonMusee>
2024-07-14 13:39:45 -0700

    Push

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,73 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers, using the function do_deploy
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.operations import run, put
-from fabric.api import env
+from fabric.api import put, run, env
 from os.path import exists
-from fabric.operations import local
-from datetime import datetime
-
-
-env.hosts = ["localhost", "100.24.238.120", "3.85.16.187"]
-env.key_filename = '~/.ssh/id_rsa'
-env.user = 'ubuntu'
-
-
-def do_pack():
-    """
-    Function to create a .tgz archive of the web_static directory
-    """
-    try:
-        local("mkdir -p versions")
-
-        tmestmp_str = datetime.now().strftime('%Y%m%d%H%M%S')
-        file_name = "web_static_" + tmestmp_str + ".tgz"
-
-        local(f"tar -cvzf versions/{file_name} web_static")
-
-        return (f"versions/{file_name}")
-
-    except Exception as e:
-        return
+env.hosts = ['100.24.238.120', '3.85.16.187']
 
 
 def do_deploy(archive_path):
-    """
-    Task/function to upload an archive, extract and deploy new version
-    of web_static
-    """
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
     try:
-        if not exists(archive_path):
-            return False
-
-        put(archive_path, "/tmp/")
-
-        archive_name = archive_path.split("/")[-1]
-        rm_extension = archive_name.split(".")[0]
-
-        remote_archive_path = "/tmp/" + archive_name
-
-        run(f"mkdir -p /data/web_static/releases/{rm_extension}")
-
-        run(f"tar -xzf {remote_archive_path} -C \
-            /data/web_static/releases/{rm_extension}")
-
-        run(f"mv /data/web_static/releases/{rm_extension}/web_static/* \
-                /data/web_static/releases/{rm_extension}/")
-
-        run(f"rm -rf /data/web_static/releases/{rm_extension}/web_static")
-
-        run(f"rm -rf {remote_archive_path}")
-
-        run("rm -rf /data/web_static/current")
-
-        run(f"ln -s /data/web_static/releases/{rm_extension} \
-           /data/web_static/current")
-
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    except Exception as e:
+    except:
         return False
